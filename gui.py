7b759a971bac38e31819ca1c2a1f6c4d7e22d8e2
Bruno Faria <bruno.fariadevl@gmail.com>
2022-04-10 11:52:15 -0300

    V2.2 -  Comentários e outra opção de calculadora - 10/04/2022

--- gui.py
+++ gui.py
@@ -2,19 +2,23 @@
 # - Criar um executável -> pyinstaller --onefile --noconsole .\gui.py
 # - Tratamento da integral
 # - Tratamento de limite superior e inferior
+# - Comentários no código
 
 from PySimpleGUI import PySimpleGUI as sg
 from sympy import *
 
 import os
 
+# Função que recebe a equação de dentro da integral e calcula a soma infinita da área (integral)
+# @param -> equacao a ser calculada
+# @return -> resultado da integral
 def calculadora(equacao):
     init_printing()
     var('x')
     result = integrate(equacao, x)
     return result
 
-# Layout
+# Layout da página mostrada para o usuário
 sg.theme('Reddit')
 layout = [
     [sg.Text('Equação'), sg.Input(key='equacao', size=(50, 50))],
@@ -22,20 +26,20 @@ layout = [
     [sg.Text('Resultado:'), sg.Text('', key='_OUTPUT_')]
 ]
 
-# Janela
+# Janela que irá ser aberta pelo usuário
 janela = sg.Window('Calculadora de Integral', layout)
 
-# Ler os eventos
+# Ler os eventos - enquanto o evento for verdadeiro a janela continuará aparecendo para o usuário
 while True:
-    eventos, valores = janela.read()
+    eventos, valores = janela.read() # Guardar valores e eventos dentro de duas variaveis, para consulta futura
     if eventos == sg.WINDOW_CLOSED:
-        break
-    if eventos == 'Calcular':
-        try:
+        break # Sai do looping fechando a janela (Encerra o programa)
+    if eventos == 'Calcular': # Caso o botão de calcular seja apertado
+        try: # Irá tentar resolver a integral e mostrar para o usuário
             calc = calculadora(valores['equacao'])
             janela.Element('_OUTPUT_').Update(calc)
-        except:
+        except: # Senão irá mostrar uma mensagem de erro para o usuário
             error_string = 'Error, escreva a equação novamente.'
             janela.Element('_OUTPUT_').Update(error_string)
-    elif eventos == 'Informações':
+    elif eventos == 'Informações':  # Caso o botão de informações seja apertado, irá mostrar informações sobre símbolos
         sg.PopupOKCancel('Elevado: ** ', 'Dividido: / ', 'Vezes: * ', 'Menos: - ', 'Soma: + ')
